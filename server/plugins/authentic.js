48531096587905cddc09199ccf7ceb4c2187176d
Jesse Hansen <twindagger@gmail.com>
2016-05-06 09:58:23 -0600

    Update to latest hapi-voucher

--- server/plugins/authentic.js
+++ server/plugins/authentic.js
@@ -1,8 +1,8 @@
 import voucher from '@leisurelink/hapi-voucher';
 import { auth } from '../env';
 
-let endpointId = auth.endpointKeyId.split('/')[0];
-let keyName = auth.endpointKeyId.split('/')[1];
+let principalId = auth.endpointKeyId.split('/')[0];
+let keyId = auth.endpointKeyId.split('/')[1];
 
 console.log(auth.authenticKeyPath);
 
@@ -12,8 +12,8 @@ export default {
     auth: {
       issuer: auth.issuer,
       audience: auth.audience,
-      endpointId: endpointId,
-      keyName: keyName,
+      principalId: principalId,
+      keyId: keyId,
       privateKeyPath: auth.endpointKeyPath
     },
     authentic: {
